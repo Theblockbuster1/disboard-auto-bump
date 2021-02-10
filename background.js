148c4744006a639a9130807f274e08f64b75a7fd
Theblockbuster1 <ben.blok1@outlook.com>
2021-02-10 13:49:18 +0000

    3.4.3 - better language support!

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var version = "3.4.2";
+var version = "3.4.3";
 
 function redirect(requestDetails) {
       return {
--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var version = "3.4.2";
+var version = "3.4.3";
 
 document.title = "Disboard Auto Bump " + version;
 document.querySelector('.version').innerHTML = '(' + version + ')';
